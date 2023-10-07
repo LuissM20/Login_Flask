5097c52eecf6dc435cc722106b59998ce1ea68ee
Luis Moreno <luism2001lopez@gmail.com>
2023-10-07 11:10:05 -0500

    Login_Flask

--- src/config.py
+++ src/config.py
@@ -1,13 +1,14 @@
 class Config:
     SECRET_KEY = 'B!1w8NAt1T^%kvhUI*S^'
 
-
+#Configuración para la conexión de la base de datos 
 class DevelopmentConfig(Config):
     DEBUG = True
     MYSQL_HOST = 'localhost'
     MYSQL_USER = 'root'
-    MYSQL_PASSWORD = '123456'
+    MYSQL_PASSWORD = ''
     MYSQL_DB = 'flask_login'
+    MYSQL_PORT=33065
 
 
 config = {
